b68e70f0b864486c86a38d24e5581b25d81779ec
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 19:59:40 -0400

    Especialista reparte tareas del ticket (v58)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v57';
+const CACHE_NAME = 'sos360-cache-v58';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
