d8538d9724ba0d5012e952f7f8d9578d1b0291ea
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 17:54:45 -0400

    Atencion de llamados y tickets ciudadanos (v52)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v51';
+const CACHE_NAME = 'sos360-cache-v52';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
