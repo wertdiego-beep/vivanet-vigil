227066bb6682f8e2867fa3352b040b14a72e2ad3
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 15:24:33 -0400

    Modulo En curso: seguimiento de estados de operativos (v45)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v44';
+const CACHE_NAME = 'sos360-cache-v45';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
