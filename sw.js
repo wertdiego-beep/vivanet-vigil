30f1c0fb4508b50c7cb933ee3d57a38bb69ba985
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 15:40:52 -0400

    Registro de operativos: bitacora con autor, hora, objetivo y cumplimiento (v47)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v46';
+const CACHE_NAME = 'sos360-cache-v47';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
