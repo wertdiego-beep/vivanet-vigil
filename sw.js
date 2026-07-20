1fb0d83e33906f2157b3e2725430ada5a80c1104
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 18:53:58 -0400

    Tickets: asignacion a especialista (v54)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v53';
+const CACHE_NAME = 'sos360-cache-v54';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
