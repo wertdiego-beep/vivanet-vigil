095b46833b63efcf27c341dcebca17869bf8a085
wertdiego-beep <wertdiego@gmail.com>
2026-07-20 19:12:26 -0400

    Personal: especialidades y grupos (v55)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v54';
+const CACHE_NAME = 'sos360-cache-v55';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
