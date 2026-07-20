2cf030c09b294e1bb745153c9dbc27bc5fc24eb8
wertdiego-beep <wertdiego@gmail.com>
2026-07-19 21:43:47 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v22';
+const CACHE_NAME = 'sos360-cache-v23';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
