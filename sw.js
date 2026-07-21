5a068051ee36888586ced428194409f0d0aa124a
wertdiego-beep <wertdiego@gmail.com>
2026-07-21 17:23:13 -0400

    Roles personalizados del jefe (v67)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v66';
+const CACHE_NAME = 'sos360-cache-v67';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
