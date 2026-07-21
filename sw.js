7d5006d878509fe0fa9ae888a157f6f57a772e1a
wertdiego-beep <wertdiego@gmail.com>
2026-07-21 17:54:07 -0400

    Semaforo SLA en tickets (v70)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v69';
+const CACHE_NAME = 'sos360-cache-v70';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
