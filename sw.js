f8617e49a81913cde1e7029c9bfb838ca00ef4c1
wertdiego-beep <wertdiego@gmail.com>
2026-07-21 15:33:21 -0400

    Fix: empresas nuevas aparecen en selector de Operadores (v62)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v61';
+const CACHE_NAME = 'sos360-cache-v62';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
