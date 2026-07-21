98d57d149bb73abee7e1a9c90d843f99d7028dd2
wertdiego-beep <wertdiego@gmail.com>
2026-07-21 17:32:28 -0400

    Scroll con mouse en barra lateral (v68)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v67';
+const CACHE_NAME = 'sos360-cache-v68';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
