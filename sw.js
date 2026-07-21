903be2969d8550e9a7476576cdd3712ae272630b
wertdiego-beep <wertdiego@gmail.com>
2026-07-21 18:01:10 -0400

    Candado de dominio anti-copia (v71)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v70';
+const CACHE_NAME = 'sos360-cache-v71';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
