66d23876a102c067ad93ca68214455a679179d45
wertdiego-beep <wertdiego@gmail.com>
2026-07-23 19:45:49 -0400

    Quitar codigo de tecnico (v77)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v76';
+const CACHE_NAME = 'sos360-cache-v77';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
