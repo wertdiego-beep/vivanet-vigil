51e2af05a9f30a9d5738dc14a24eddc68d1c2fee
wertdiego-beep <wertdiego@gmail.com>
2026-07-23 19:57:20 -0400

    Quitar pestana Equipo tecnico (v78)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v77';
+const CACHE_NAME = 'sos360-cache-v78';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
