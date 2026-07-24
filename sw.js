3026958bdf7e364a6ecc4e96bcaf614ca32cd51d
wertdiego-beep <wertdiego@gmail.com>
2026-07-23 20:09:48 -0400

    Simplificar seguridad: quitar clave de coaccion (v79)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v78';
+const CACHE_NAME = 'sos360-cache-v79';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
