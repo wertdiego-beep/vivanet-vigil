fa03f724fcd0136df4d1aec30f9339c4c4b26b04
wertdiego-beep <wertdiego@gmail.com>
2026-07-24 18:34:25 -0400

    Fixes operador: avisos push feedback, radio asistencia, anti-solape visitas (v87)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v86';
+const CACHE_NAME = 'sos360-cache-v87';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
