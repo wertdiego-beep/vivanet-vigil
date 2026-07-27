748d410af6ba8806e32e9147c54186ad996c2e7c
wertdiego-beep <wertdiego@gmail.com>
2026-07-27 18:29:24 -0400

    Reportes georreferenciados en el mapa de Monitoreo (v91)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v90';
+const CACHE_NAME = 'sos360-cache-v91';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
