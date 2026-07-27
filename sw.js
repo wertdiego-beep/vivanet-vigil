413de86a5d45beaae1ad3cbc573a4c8b984cb3cc
wertdiego-beep <wertdiego@gmail.com>
2026-07-27 18:04:15 -0400

    Noticias del sector: seccion cliente + publicador maestro (v89)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v88';
+const CACHE_NAME = 'sos360-cache-v89';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
