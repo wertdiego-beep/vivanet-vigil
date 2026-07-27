df6547cecce08d6d349f463e8fc2f6a4e5a91ab0
wertdiego-beep <wertdiego@gmail.com>
2026-07-27 18:59:34 -0400

    Interruptor noticias por empresa + gating de plan (v92)

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v91';
+const CACHE_NAME = 'sos360-cache-v92';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
