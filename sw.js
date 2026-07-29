18c21346ff974c9ba5d6ca47e5d4d9259b201dbc
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 19:19:15 -0400

    v117: ingreso en dos pasos, primero elegir y despues el correo

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v116';
+const CACHE_NAME = 'sos360-cache-v117';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
