b340364c100114acdeab6955970df812e47187c6
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 16:41:48 -0400

    v109: el jefe solo ve en Roles los permisos que la plataforma le designo

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v108';
+const CACHE_NAME = 'sos360-cache-v109';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
