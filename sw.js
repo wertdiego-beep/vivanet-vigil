267bed2696e2b431abca1c1e7ffea9662a22f1e3
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 18:52:11 -0400

    v115: jefes en la lista de operadores + permisosPlan

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v114';
+const CACHE_NAME = 'sos360-cache-v115';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
