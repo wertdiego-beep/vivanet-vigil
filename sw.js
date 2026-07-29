74753358f1dfb89d01e48a701bcc5b8178879522
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 19:08:19 -0400

    v116: boton volver en cada paso del registro del cliente

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v115';
+const CACHE_NAME = 'sos360-cache-v116';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
