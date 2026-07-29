cddb9014cedc307198db450c2607d1f29a93afb9
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 18:42:54 -0400

    v114: fix panel en blanco al cortar KPI + opGo a prueba de vistas fantasma

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v113';
+const CACHE_NAME = 'sos360-cache-v114';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
