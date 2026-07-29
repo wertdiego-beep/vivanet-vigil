ff9ae833d988cc25586e64e2d5635ec4cd7c21c2
wertdiego-beep <wertdiego@gmail.com>
2026-07-29 16:31:23 -0400

    v108: iconos de permisos en grilla 7x2 dentro de la tarjeta del operador

--- sw.js
+++ sw.js
@@ -44,7 +44,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'sos360-cache-v107';
+const CACHE_NAME = 'sos360-cache-v108';
 const APP_SHELL = [
   '/manifest.json',
   '/icon-192.png',
